9f2b43c36ff97f03c61e4ef2e8f94039304a9bfc
Enes Akıllıoksuz <114690203+cleoppa@users.noreply.github.com>
2023-03-23 22:18:54 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,43 +1,43 @@
-﻿-- frosty development -- 
+-- frosty development -- 
 
-local kutuYetki = "OzelYetki"
+local kutuYetki = "BoxAuthority"
 
 function table.random ( theTable )
     return theTable[math.random ( #theTable )]
 end
 
-function kutuDagit(thePlayer,cmd)
+function boxDistribute(thePlayer,cmd)
 	for _, oyuncu in ipairs(getElementsByType("player")) do
-		if isObjectInACLGroup("user."..getAccountName(getPlayerAccount(thePlayer)), aclGetGroup(kutuYetki))then
-			setElementData(oyuncu, "kutu:sure", true)
-			outputChatBox(">> #ffffffBir yetkili herkese kutu dağıttı, /kutuac ile kutunuzu açabilirsin.", oyuncu, 0, 255, 0, true)
-			outputChatBox(">> #ffffff3 dakika içerisinde kutu isteğini onaylamazsan kutu silinecek!", oyuncu, 0, 0, 255, true)
-			triggerClientEvent(oyuncu, "playKutuSound", oyuncu)
+		if isObjectInACLGroup("user."..getAccountName(getPlayerAccount(thePlayer)), aclGetGroup(BoxAuthority))then
+			setElementData(oyuncu, "box:time", true)
+			outputChatBox(">> #ffffffAn official has distributed boxes to everyone, you can open your box with /kutuac.", oyuncu, 0, 255, 0, true)
+			outputChatBox(">> #ffffff If you don't approve the box request within 3 minutes, the box will be deleted!", oyuncu, 0, 0, 255, true)
+			triggerClientEvent(player, "playKutuSound", player)
 			setTimer(function()
-				if getElementData(oyuncu, "kutu:sure") == true then
-					outputChatBox(">> #ffffffKutu isteğini onaylamadığın için kutu silindi.", oyuncu, 255, 0, 0, true)
-					setElementData(oyuncu, "kutu:sure", false)
+				if getElementData(player, "box:time") == true then
+					outputChatBox(">> #ffffffThe box has been deleted because you did not approve the box request.", oyuncu, 255, 0, 0, true)
+					setElementData(player, "box:time", false)
 				end
 			end, 180000, 1)
 		else
-			outputChatBox(">> #ffffffSen yetkili misin aptal ucube.", thePlayer, 255, 0, 0, true)
+			outputChatBox(">> #ffffffAre you in charge you stupid freak.", thePlayer, 255, 0, 0, true)
 		end
 	end
 end
-addCommandHandler("kutudagit", kutuDagit)
+addCommandHandler("thebox", boxDistribute)
 
-function kutuOnayla(thePlayer, cmd)
-	if not getElementData(thePlayer, "kutu:sure") == true then
-		outputChatBox(">> #ffffffKutu isteğin bulunamadı, kutu dağıtılmasını bekle.", thePlayer, 255, 0, 0, true)
+function boxConfirm(thePlayer, cmd)
+	if not getElementData(thePlayer, "box:time") == true then
+		outputChatBox(">> #ffffff Your box request was not found, wait for the box to be deployed.", thePlayer, 255, 0, 0, true)
 	return end
-	local para = {5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000, 2000000}
-	local randomPara = table.random(para)
-	givePlayerMoney(thePlayer, randomPara)
-	setElementData(thePlayer, "kutu:sure", false)
-	outputChatBox(">> #ffffffKutu isteğini onayladın  ve içerisinden "..formatMoney(randomPara).."$ para çıktı!", thePlayer, 0, 255, 0, true)
+	local money = {5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000, 2000000}
+	local randomMoneyy = table.random(money)
+	givePlayerMoney(thePlayer, randomMoneyy)
+	setElementData(thePlayer, "box:time", false)
+	outputChatBox(">> You approved the #ffffffBox request and it contained "..formatMoney(randomPara).."$ money!", thePlayer, 0, 255, 0, true)
 	triggerClientEvent(thePlayer, "stopKutuSound", thePlayer)
 end
-addCommandHandler("kutuac", kutuOnayla)
+addCommandHandler("boxopen", boxConfirm)
 
 function formatMoney(amount)
     local left,num,right = string.match(tostring(amount),'^([^%d]*%d)(%d*)(.-)$')
